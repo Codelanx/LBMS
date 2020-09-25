945d18e934724771619ebf5446719200049f7b85
Spencer Alderman <rogue@codelanx.com>
2020-09-25 16:08:08 -0400

    Add Author, though I haven't started it yet

---
+++ src/main/java/edu/rit/codelanx/data/types/Author.java
@@ -0,0 +1,16 @@
+package edu.rit.codelanx.data.types;
+
+import edu.rit.codelanx.data.state.State;
+
+//TODO
+public class Author implements State {
+    @Override
+    public long getID() {
+        return 0;
+    }
+
+    @Override
+    public Type getType() {
+        return null;
+    }
+}
