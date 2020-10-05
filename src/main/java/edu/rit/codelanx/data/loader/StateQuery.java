f66a4560b28a7f850d236b9e3c3f39af09973b00
Spencer Alderman <rogue@codelanx.com>
2020-10-05 19:23:14 -0400

    Add query system

---
+++ src/main/java/edu/rit/codelanx/data/loader/StateQuery.java
@@ -0,0 +1,252 @@
+package edu.rit.codelanx.data.loader;
+
+import com.codelanx.commons.data.SQLBiFunction;
+import edu.rit.codelanx.data.DataStorage;
+import edu.rit.codelanx.data.state.State;
+import edu.rit.codelanx.data.state.types.StateType;
+import edu.rit.codelanx.data.storage.StateStorage;
+import edu.rit.codelanx.data.storage.StorageContainer;
+import edu.rit.codelanx.data.storage.field.DataField;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StateQuery<S extends State> implements Query<S> {
+
+    private final DataStorage storage;
+    private final Class<S> type;
+    private final List<Comparison<?>> comparisons = new ArrayList<>();
+    private final List<Predicate<S>> postFixes = new ArrayList<>();
+
+    public StateQuery(DataStorage storage, Class<S> type) {
+        this.storage = storage;
+        this.type = type;
+    }
+
+    @Override
+    public Stream<S> results() {
+        Stream<S> s = this.storage.getAdapter().handleQuery(this);
+        for (Predicate<S> pred : this.postFixes) {
+            s = s.filter(pred);
+        }
+        return s;
+    }
+
+    public Class<S> getType() {
+        return this.type;
+    }
+
+    public <E> Query<S> predicate(DataField<E> field, E value, ComparisonType type) {
+        this.comparisons.add(new Comparison<>(field, value, type));
+        return this;
+    }
+
+    public List<Comparison<?>> getComparisons() {
+        return Collections.unmodifiableList(this.comparisons);
+    }
+
+    @Override
+    public <E> Query<S> filterBy(DataField<E> field, Predicate<E> test) {
+        this.postFixes.add(state -> test.test(field.get(state)));
+        return this;
+    }
+
+    @Override
+    public <E> Query<S> isEqual(DataField<E> field, E value) {
+        return this.predicate(field, value, ComparisonType.EQUALITY);
+    }
+
+    @Override
+    public <E> Query<S> isAny(DataField<E> field, Iterable<E> values) {
+        this.comparisons.add(new MultiComparison<>(field, values));
+        return this;
+    }
+
+    @Override
+    public <E extends Comparable<E>> Query<S> isLessThan(DataField<E> field, E value) {
+        return this.predicate(field, value, ComparisonType.LESS_THAN);
+    }
+
+    @Override
+    public <E extends Comparable<E>> Query<S> isGreaterThan(DataField<E> field, E value) {
+        return this.predicate(field, value, ComparisonType.GREATER_THAN);
+    }
+
+    @Override
+    public <E extends Comparable<E>> Query<S> isLessThanOrEq(DataField<E> field, E value) {
+        return this.predicate(field, value, ComparisonType.LESS_THAN_OR_EQ);
+    }
+
+    @Override
+    public <E extends Comparable<E>> Query<S> isGreaterThanOrEq(DataField<E> field, E value) {
+        return this.predicate(field, value, ComparisonType.GREATER_THAN_OR_EQ);
+    }
+
+    public enum ComparisonType {
+        EQUALITY("=", Objects::equals),
+        LESS_THAN("<", (o1, o2) -> o1.compareTo(o2) < 0),
+        GREATER_THAN(">", (o1, o2) -> o1.compareTo(o2) > 0),
+        LESS_THAN_OR_EQ("<=", (o1, o2) -> o1.compareTo(o2) <= 0),
+        GREATER_THAN_OR_EQ(">=", (o1, o2) -> o1.compareTo(o2) >= 0),
+        IS_ANY("IN"),
+        ;
+
+        private final String operator;
+        private final BiPredicate<Object, Object> compare;
+
+        private <T extends Comparable<T>> ComparisonType(String operator, BiPredicate<T, T> compare) {
+            this.operator = operator;
+            //warning left, this isn't safe if you use it incorrectly
+            //(should only get comparables unless IS_ANY or EQUALITY)
+            this.compare = (BiPredicate<Object, Object>) (BiPredicate<?, ?>) compare;
+        }
+
+        private ComparisonType(String operator) {
+            this(operator, null);
+        }
+
+        public String getOperator() {
+            return this.operator;
+        }
+
+        public <T> boolean compare(T one, T two) {
+            if (this == IS_ANY) {
+                throw new UnsupportedOperationException("Cannot calculate comparison for a one-to-many relationship");
+            }
+            if (this != EQUALITY && !(one instanceof Comparable)) {
+                throw new IllegalArgumentException("Cannot relatively compare a non-comparable class " + one.getClass().getSimpleName());
+            }
+            return this.compare.test(one, two);
+        }
+    }
+
+    public class Comparison<E> {
+
+        private final DataField<E> field;
+        private final E value;
+        private final ComparisonType type;
+
+        public Comparison(DataField<E> field, E value, ComparisonType type) {
+            this.field = field;
+            this.value = value;
+            this.type = type;
+        }
+
+        public DataField<E> getDataField() {
+            return this.field;
+        }
+        public E getValue() {
+            return this.value;
+        }
+        public ComparisonType getType() {
+            return this.type;
+        }
+
+        public boolean test(State s) {
+            return this.getType().compare(this.getDataField().get(s), this.value);
+        }
+
+        //checks for states if it's possible to find them through our cache with 100% certainly on the result
+        //otherwise, an uncertain result returns null, indicating the cache should be skipped
+        public Stream<S> findStates() {
+            if (this.getDataField().isKey()
+                && this.getDataField().isUnique()
+                && this.getType() == ComparisonType.EQUALITY) {
+                //would fail if passed an unrelated data field
+                //TODO: We should probably find a way to define a state->field association
+                //TODO:     However, I want to avoid over-generifying
+                return (Stream<S>) this.getDataField().findStatesByValue(this.getValue());
+            }
+            return null;
+        }
+
+        public String valueToString() {
+            return Objects.toString(this.value);
+        }
+
+        public String toSQLPredicate() {
+            return this.field.getName() + ' ' + this.getType().getOperator() + ' ' + this.valueToString();
+        }
+
+        public String toPreparedSQL() {
+            return this.field.getName() + ' ' + this.getType().getOperator() + " ?";
+        }
+
+        public boolean isPreparable() {
+            return true;
+        }
+
+    }
+
+    public class MultiComparison<E> extends Comparison<E> {
+
+        private final Set<E> values;
+
+        public MultiComparison(DataField<E> field, Iterable<E> values) {
+            super(field, null, ComparisonType.IS_ANY);
+            this.values = StreamSupport.stream(values.spliterator(), false).collect(Collectors.toSet());
+        }
+
+        @Override
+        public boolean test(State s) {
+            E val = this.getDataField().get(s);
+            return this.values.contains(val);
+        }
+
+        @Override
+        public String toPreparedSQL() {
+            return super.toSQLPredicate(); //aka don't add to prepared statement, unsupported
+        }
+
+        @Override
+        public String valueToString() {
+            return "(" + this.values.stream().map(Object::toString).collect(Collectors.joining(",")) + ")";
+        }
+
+        @Override
+        public boolean isPreparable() {
+            return false;
+        }
+    }
+
+    Stream<S> locateLocal(StateStorage<S> storage) {
+        Stream<S> cached = this.comparisons.stream()
+                .map(Comparison::findStates)
+                .findFirst().orElse(null);
+        if (cached != null) {
+            return cached;
+        }
+        //TODO: old code below, can be made faster by using keys properly etc
+        return storage.streamLoaded()
+                .filter(s -> this.getComparisons().stream().allMatch(c -> c.test(s)));
+    }
+
+    Stream<S> runSQLQuery(SQLBiFunction<String, Object[], Stream<S>> create) throws SQLException {
+        //yada yada sql injection
+        //at the very least, we're only trusting ourselves.
+        //I suppose even that could be dangerous, but still
+        Class<? extends State> type = this.getType();
+        State.Type stateType = StateType.fromClass(type);
+        StorageContainer container = type.getAnnotation(StorageContainer.class);
+        if (stateType == null || container == null) {
+            throw new IllegalArgumentException("Unknown container for type: " + type.getSimpleName());
+        }
+        //TODO Think of a less hacky way to build an sql query here?
+        String sqlWhere = this.getComparisons().stream()
+                .map(Comparison::toPreparedSQL)
+                .collect(Collectors.joining());
+        Object[] args = this.getComparisons().stream()
+                .filter(Comparison::isPreparable)
+                .toArray(Object[]::new);
+        return create.apply("SELECT * FROM " + container.value() + " WHERE " + sqlWhere, args);
+    }
+}
--- src/main/java/edu/rit/codelanx/data/loader/StorageAdapter.java
+++ src/main/java/edu/rit/codelanx/data/loader/StorageAdapter.java
@@ -1,17 +1,29 @@
 package edu.rit.codelanx.data.loader;
 
+import edu.rit.codelanx.data.DataStorage;
 import edu.rit.codelanx.data.state.State;
-import edu.rit.codelanx.data.state.StateBuilder;
-import edu.rit.codelanx.data.types.Library;
+import edu.rit.codelanx.data.state.types.Library;
+import edu.rit.codelanx.data.storage.field.DataField;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.stream.Stream;
 
 public interface StorageAdapter {
 
-    public Library getLibrary();
-    public <R extends State> List<R> getState(Class<R> type);
+    public Library getLibrary(); //this is essentially a primary class
     public <R extends State> R insert(StateBuilder<R> builder);
     public void loadAll() throws IOException;
+    public void saveAll() throws IOException;
     //TODO: Update/save
+
+    public <R extends State> Stream<R> handleQuery(StateQuery<R> query);
+    public <R extends State> R loadState(long id, Class<R> type);
+    public <R extends State, E> Stream<R> loadState(Class<R> type, DataField<E> field, E value);
+    //notified when a change occurs, along with the new value
+    // May also happen if someone calls Field#mutate, and the Field points to a
+    // State which is simply mutated (not re-referenced)
+    public <E> void notifyUpdate(State state, DataField<E> field, E value);
+
+    public void remove(State state);
+    public boolean isCached(); //true if we keep values loaded from program start to finish (e.g. flatfiles)
 }
