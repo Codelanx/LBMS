8d466f95ea1730b1c09e69e6a0df673079763ae1
Spencer Alderman <rogue@codelanx.com>
2020-10-05 19:15:19 -0400

    Flesh out State, remove UpdatableState / ResultantState. State.Type is now an interface, described by StateType

--- src/main/java/edu/rit/codelanx/data/state/State.java
+++ src/main/java/edu/rit/codelanx/data/state/State.java
@@ -1,19 +1,39 @@
 package edu.rit.codelanx.data.state;
 
+import com.codelanx.commons.data.SQLBiFunction;
+import edu.rit.codelanx.data.DataStorage;
+import edu.rit.codelanx.data.loader.InputMapper;
+import edu.rit.codelanx.data.loader.StateBuilder;
+import edu.rit.codelanx.data.storage.field.DataField;
+import edu.rit.codelanx.data.state.types.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
+
 public interface State {
 
-    public long getID(); //returns the ID for the relevant state, helps with indexing
+    public DataField<Long> getIDField();
+    public DataField<? super Object>[] getFields();
     public Type getType();
     public String toFormattedText();
-    public enum Type {
-        BOOK,
-        CHECKOUT,
-        LIBRARY,
-        TRANSACTION,
-        VISIT,
-        VISITOR,
-        AUTHOR,
-        UNKNOWN,
-        ;
+    public DataStorage getLoader();
+    public boolean isValid();
+    public void unload(); //unloads this value, invalidating it
+    //returns the ID for the relevant state, helps with indexing
+    default public long getID() {
+        return this.getIDField().get(this);
+    }
+
+    public interface Type {
+
+        public String getName();
+        public <T extends State> Class<T> getConcreteType(); //bit dangerous doing it like this
+        public State mapFromSQL(DataStorage storage, ResultSet set) throws SQLException;
+        public State mapFromFile(DataStorage storage, Map<String, Object> file);
+        public long getNextID();
+
     }
 }
---
+++ src/main/java/edu/rit/codelanx/data/state/types/StateType.java
@@ -0,0 +1,93 @@
+package edu.rit.codelanx.data.state.types;
+
+import com.codelanx.commons.data.SQLBiFunction;
+import edu.rit.codelanx.data.DataStorage;
+import edu.rit.codelanx.data.loader.InputMapper;
+import edu.rit.codelanx.data.loader.StateBuilder;
+import edu.rit.codelanx.data.state.State;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
+
+public enum StateType implements State.Type {
+    BOOK(Book.class, Book::new, Book::new, Book::new),
+    CHECKOUT(Checkout.class, Checkout::new, Checkout::new, Checkout::new),
+    LIBRARY(Library.class, Library::new, Library::new, Library::new),
+    TRANSACTION(Transaction.class, Transaction::new, Transaction::new, Transaction::new),
+    VISIT(Visit.class, Visit::new, Visit::new, Visit::new),
+    VISITOR(Visitor.class, Visitor::new, Visitor::new, Visitor::new),
+    AUTHOR(Author.class, Author::new, Author::new, Author::new),
+    AUTHOR_LISTING(AuthorListing.class, AuthorListing::new, AuthorListing::new, AuthorListing::new),
+    //UNKNOWN is unimplemented
+    ;
+
+    private static final StateType[] VALUES = StateType.values();
+    private final AtomicLong autoID = new AtomicLong(1);
+    private final Class<? extends State> type;
+    private final StateBuilder.StateConstructor<?> builderBlueprint;
+    private final SQLBiFunction<DataStorage, ResultSet, ? extends State> sqlBuild;
+    private final BiFunction<DataStorage, Map<String, Object>, ? extends State> fileBuild;
+
+    private <T extends State> StateType(Class<T> type,
+                 StateBuilder.StateConstructor<T> blueprint,
+                 SQLBiFunction<DataStorage, ResultSet, T> sqlBuild,
+                 BiFunction<DataStorage, Map<String, Object>, T> fileBuild) {
+        this.type = type;
+        this.builderBlueprint = blueprint;
+        this.sqlBuild = sqlBuild;
+        this.fileBuild = fileBuild;
+    }
+
+    public void setAutoIncrementID(long value) {
+        this.autoID.set(value);
+    }
+
+    @Override
+    public long getNextID() {
+        return this.autoID.getAndIncrement();
+    }
+
+    @Override
+    public String getName() {
+        return this.name().toLowerCase();
+    }
+
+    //warnings not suppressed here, this is actually a little dangerous
+    @Override
+    public <T extends State> Class<T> getConcreteType() {
+        return (Class<T>) this.type; //will immediately CCE if invalidly used
+    }
+
+    @Override
+    public State mapFromSQL(DataStorage storage, ResultSet set) throws SQLException {
+        return this.sqlBuild.apply(storage, set);
+    }
+
+    @Override
+    public State mapFromFile(DataStorage storage, Map<String, Object> file) {
+        return this.fileBuild.apply(storage, file);
+    }
+
+    public static State.Type fromClass(Class<? extends State> stateType) {
+        for (StateType t : VALUES) {
+            if (t.type == stateType) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public static State.Type fromClassStrict(Class<?> type) {
+        State.Type back = null;
+        if (InputMapper.isStateClass(type)) {
+            back = StateType.fromClass((Class<? extends State>) type);
+        }
+        if (back == null) {
+            throw new IllegalArgumentException("Cannot locate type for state: " + type.getName());
+        }
+        return back;
+    }
+}
