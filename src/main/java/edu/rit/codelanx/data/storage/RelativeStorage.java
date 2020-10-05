c6e81a25223e86fe2656575cb584142a7eb3184a
Spencer Alderman <rogue@codelanx.com>
2020-10-05 19:18:01 -0400

    Add in RelativeStorage / StateStorage to keep persistant references (DataField will not persist an object indefinitely).

---
+++ src/main/java/edu/rit/codelanx/data/storage/RelativeStorage.java
@@ -0,0 +1,38 @@
+package edu.rit.codelanx.data.storage;
+
+import edu.rit.codelanx.data.DataStorage;
+import edu.rit.codelanx.data.state.State;
+import edu.rit.codelanx.data.state.types.StateType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RelativeStorage {
+
+    private final Map<Class<?>, StateStorage<?>> states = new HashMap<>();
+    private final DataStorage storage;
+
+    public RelativeStorage(DataStorage storage) {
+        this.storage = storage;
+        for (State.Type type : StateType.values()) {
+            this.states.put(type.getConcreteType(), new StateStorage<>(type, storage));
+        }
+    }
+
+    public void addState(State state) {
+        this.<State>getStateStorage(state.getClass()).addState(state);
+    }
+
+    @SuppressWarnings("unchecked") //one Storage<T> per Class<T>
+    public <T extends State> StateStorage<T> getStateStorage(Class<? extends T> stateType) {
+        State.Type t = StateType.fromClass(stateType);
+        if (t == null) {
+            throw new IllegalArgumentException("Unknown state type: " + stateType.getName());
+        }
+        return (StateStorage<T>) this.states.computeIfAbsent(stateType, k -> new StateStorage<T>(t, this.storage));
+    }
+
+    public StateStorage<?> getStateStorage(State.Type type) {
+        return this.getStateStorage(type.getConcreteType());
+    }
+}
---
+++ src/main/java/edu/rit/codelanx/data/storage/StateStorage.java
@@ -0,0 +1,102 @@
+package edu.rit.codelanx.data.storage;
+
+import edu.rit.codelanx.data.DataStorage;
+import edu.rit.codelanx.data.state.State;
+import edu.rit.codelanx.data.storage.field.DataField;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.WeakHashMap;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+//stores hard references for states, where they won't go out of scope (unless we want them to)
+public class StateStorage<T extends State> {
+
+    private final Map<Long, T> loaded = new WeakHashMap<>();
+    private final State.Type type;
+    private final Class<T> concreteType;
+    private final DataStorage storage;
+
+    public StateStorage(State.Type type, DataStorage storage) {
+        this.type = type;
+        this.concreteType = this.type.getConcreteType();
+        this.storage = storage;
+    }
+
+    public T getByID(long id) {
+        //TODO: Load if not loaded
+        return this.loaded.computeIfAbsent(id, k -> {
+            return this.storage.getAdapter().loadState(k, this.concreteType);
+        });
+    }
+
+    public T computeIfAbsent(long id, Supplier<T> state) {
+        return this.loaded.computeIfAbsent(id, k -> state.get());
+    }
+
+    // We quite literally do a check for T
+    @Deprecated
+    public <E> T getUniqueValue(DataField<E> field, E value) {
+        if (!field.isUnique()) {
+            throw new IllegalStateException("Cannot retrieve a unique value from a non-keyed field");
+        }
+        if (field.isKey()) {
+            T s = (T) field.findStatesByValue(value).findAny().orElse(null);
+            if (s == null) {
+                //not loaded?
+                s = this.storage.getAdapter().loadState(this.concreteType, field, value).findAny().orElse(null);
+                if (s != null && this.concreteType.isAssignableFrom(s.getClass())) {
+                    //found by indexing, whew
+                    this.loaded.put(s.getID(), (T) s);
+                    return (T) s;
+                }
+            } else if (!this.concreteType.isAssignableFrom(s.getClass())) {
+                throw new IllegalArgumentException("Wrong state type returned from index search");
+            }
+        }
+        //searches everything else - slower if not indexed
+        Predicate<E> test = value == null ? Objects::isNull : value::equals;
+        return this.loaded.values().stream()
+                .filter(s ->  test.test(field.get(s)))
+                .findAny().orElseGet(() -> {
+                    T back = this.storage.query(this.concreteType)
+                            .isEqual(field, value)
+                            .results().findAny().orElse(null);
+                    if (back == null) {
+                        return null;
+                    }
+                    this.loaded.put(back.getID(), back);
+                    return back;
+                });
+
+    }
+
+    public Stream<T> streamLoaded() {
+        return this.loaded.values().stream();
+    }
+
+    public void addState(T state) {
+        this.loaded.put(state.getID(), state);
+    }
+
+    public void release(T state) {
+        if (this.storage.getAdapter().isCached()) {
+            return;
+        }
+        this.loaded.remove(state.getID());
+        Arrays.stream(state.getFields()).forEach(d -> d.forget(state));
+    }
+
+    public void remove(T state) {
+        this.storage.getAdapter().remove(state);
+        this.release(state);
+    }
+
+    public void forAllLoaded(Consumer<T> consumer) {
+        this.loaded.values().forEach(consumer);
+    }
+}
