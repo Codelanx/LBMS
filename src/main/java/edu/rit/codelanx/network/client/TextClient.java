974d3d9bfdcf8340674dc8d0afe88b77c364f7b6
Amy Do <ahd6901@rit.edu>
2020-10-07 18:32:06 -0400

    documentation added for CommandExecutor, TextClient, Message, Messenger

--- src/main/java/edu/rit/codelanx/network/client/TextClient.java
+++ src/main/java/edu/rit/codelanx/network/client/TextClient.java
@@ -1,6 +1,7 @@
 package edu.rit.codelanx.network.client;
 
 import edu.rit.codelanx.data.state.State;
+import edu.rit.codelanx.network.io.Message;
 import edu.rit.codelanx.network.io.Messenger;
 import edu.rit.codelanx.network.io.TextMessage;
 import edu.rit.codelanx.network.server.Server;
@@ -27,12 +28,6 @@ public class TextClient implements Client<TextMessage> {
     private final BufferedReader buffer;
     private final PrintStream output;
     private volatile WeakReference<Server<TextMessage>> server;
-    /*
-    public final class System {
-    public static final InputStream in;
-    public static final PrintStream out;
-    public static final PrintStream err;
-     */
 
     public TextClient(InputStream input, PrintStream output) {
         this.reader = new InputStreamReader(input);
@@ -44,7 +39,7 @@ public class TextClient implements Client<TextMessage> {
      * {@inheritDoc}
      *
      * @param server {@inheritDoc}
-     * 
+     *
      * @see Client#connect(Server)
      */
     @Override
@@ -53,8 +48,7 @@ public class TextClient implements Client<TextMessage> {
     }
 
     /**
-     * Begins reading input from the terminal, until a {@code null} is
-     * read
+     * Begins reading input from the terminal, until a {@code null} is read
      *
      * @throws IOException {@inheritDoc}
      * @see Client#display()
@@ -72,17 +66,29 @@ public class TextClient implements Client<TextMessage> {
         }
     }
 
+    /**
+     * terminates the application when called
+     * @throws Exception- when errors occur
+     */
     @Override
     public void close() throws Exception {
         this.buffer.close();
         this.reader.close();
     }
 
+    /**
+     * print out the message to user via terminal
+     * @param message- to be printed out
+     */
     @Override
     public void sendMessage(String message) {
         this.output.println(message);
     }
 
+    /**
+     * print out the user' states via terminal
+     * @param states -array of states
+     */
     @Override
     public void renderState(State... states) {
         Arrays.stream(states)
@@ -90,6 +96,12 @@ public class TextClient implements Client<TextMessage> {
                 .forEach(this.output::println); //final code version
     }
 
+    /**
+     * prints out the text message from a messenger to the terminal
+     * @throws UnsupportedOperationException if text message come from a non-server messenger
+     * @param from-The {@link Messenger} we received a message from
+     * @param message-The received {@link Message}
+     */
     @Override
     public void receive(Messenger<TextMessage> from, TextMessage message) {
         if (!(from instanceof Server)) { //extra safety check, necessary?
--- src/main/java/edu/rit/codelanx/network/io/Messenger.java
+++ src/main/java/edu/rit/codelanx/network/io/Messenger.java
@@ -1,9 +1,23 @@
 package edu.rit.codelanx.network.io;
 
+/**
+ * Provides an interface representing a messenger. This can be either internal system or a client
+ * @param <M>-type of Message being exchanged
+ * @author sja9291  Spencer Alderman
+ */
 public interface Messenger<M extends Message<?>> {
-
+    /**
+     * gets the message and process it
+     * @param from-The {@link Messenger} we received a message from
+     * @param message-The received {@link Message}
+     */
     public void receive(Messenger<M> from, M message);
 
+    /**
+     * sends the message to a particular messenger
+     * @param other- the {@link Messenger} to receive the message
+     * @param message- the {@link Message} to be sent
+     */
     default public void message(Messenger<M> other, M message) {
         other.receive(this, message);
     }
