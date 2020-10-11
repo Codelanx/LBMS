30224d40c132daa948d90446b9f83f0f8894f876
Spencer Alderman <rogue@codelanx.com>
2020-10-11 19:47:25 -0400

    Allow specifying the output stream for reporting errors

--- src/main/java/edu/rit/codelanx/util/Errors.java
+++ src/main/java/edu/rit/codelanx/util/Errors.java
@@ -1,11 +1,19 @@
 package edu.rit.codelanx.util;
 
+import java.io.PrintStream;
+
 public enum Errors {;
 
     public static void report(Throwable t) {
         //no different than the generated code at the moment,
         //but will let us be nicer with the output of errors
-        t.printStackTrace();
+        Errors.report(t, System.err);
+    }
+
+    public static void report(Throwable t, PrintStream out) {
+        //no different than the generated code at the moment,
+        //but will let us be nicer with the output of errors
+        t.printStackTrace(out);
     }
 
     public static void reportAndExit(Throwable t) throws RuntimeException {
