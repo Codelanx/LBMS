52ce510005e14843939bf9d5e1495a0c871aae28
Spencer Alderman <rogue@codelanx.com>
2020-10-12 16:20:20 -0400

    missed documenting a method in Server

--- src/main/java/edu/rit/codelanx/network/server/Server.java
+++ src/main/java/edu/rit/codelanx/network/server/Server.java
@@ -19,17 +19,18 @@ import edu.rit.codelanx.data.state.types.Library; //imported solely for javadocs
 public interface Server<T extends Message<?>> extends Messenger<T> {
 
     /**
-     * Returns the {@link Interpreter}, which decides how to act
-     * upon a received {@link Message<T>}
+     * The {@link Interpreter}, which decides how to act upon receiving a
+     * {@link Message<T>}
      *
      * @return The held {@link Interpreter} for this server
      * @see Interpreter
+     * @see Messenger#receive(Messenger, Message)
      */
     public Interpreter getInterpreter();
 
     /**
-     * Returns the {@link DataSource} which holds all of the library's
-     * data, relevant to customers and books owned within the library
+     * The {@link DataSource} which holds all of the library's data, relevant
+     * to customers and books owned within the library
      *
      * @return A {@link DataSource} responsible for holding the library's
      *         current data
@@ -38,8 +39,8 @@ public interface Server<T extends Message<?>> extends Messenger<T> {
     public DataSource getDataStorage();
 
     /**
-     * Returns the {@link DataSource} which refences the "Book Store", aka
-     * an emulated online shop which you can query in a similar fashion to
+     * The {@link DataSource} which refences the "Book Store", aka an emulated
+     * online shop which you can query in a similar fashion to
      * {@link #getDataStorage()}, and which provides the available books to
      * be added to a {@link Library}, as they are represented in the
      * {@code books.txt} resource
@@ -49,5 +50,12 @@ public interface Server<T extends Message<?>> extends Messenger<T> {
      * @see DataSource
      */
     public DataSource getBookStore();
+
+    /**
+     * The {@link Clock} that runs server-side events, like opening and
+     * closing the {@link Library} instances
+     *
+     * @return The currently used {@link Clock}
+     */
     public Clock getClock();
 }
