b6e79175a7aa197824563e1f404a213f80d9fba5
Mark Anderson <mande984@gmail.com>
2020-11-13 16:53:05 -0500

    Finished tests for ReturnCommand
    Signed-off-by: Mark Anderson <mande984@gmail.com>

--- src/test/java/edu/rit/codelanx/cmd/cmds/TestReturnCommand.java
+++ src/test/java/edu/rit/codelanx/cmd/cmds/TestReturnCommand.java
@@ -2,20 +2,23 @@ package edu.rit.codelanx.cmd.cmds;
 
 import edu.rit.codelanx.cmd.CommandExecutor;
 import edu.rit.codelanx.cmd.ResponseFlag;
+import edu.rit.codelanx.data.DataSource;
 import edu.rit.codelanx.data.state.types.Book;
+import edu.rit.codelanx.data.state.types.Checkout;
+import edu.rit.codelanx.data.state.types.Library;
 import edu.rit.codelanx.data.state.types.Visitor;
 import edu.rit.codelanx.network.io.TextMessage;
 import edu.rit.codelanx.network.server.Server;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.util.Collections;
-
-import static org.mockito.Matchers.any;
 
 public class TestReturnCommand {
 
@@ -33,7 +36,8 @@ public class TestReturnCommand {
     public void init() {
         MockitoAnnotations.initMocks(this);
         this.ret = new ReturnCommand(this.servMock);
-        retSpy = Mockito.spy(ret);
+        this.retSpy = Mockito.spy(ret);
+
     }
 
     @Test
@@ -56,6 +60,7 @@ public class TestReturnCommand {
         Expectation: All inputs should be able to be handled, no book should
         be returned
          */
+        Mockito.doReturn(null).when(retSpy).queryVisitor(Matchers.anyLong());
         Assertions.assertSame(ResponseFlag.SUCCESS,
                 retSpy.onExecute(this.execMock, "2", "1"));
         Mockito.verify(execMock).sendMessage("return,invalid-visitor-id;");
@@ -68,10 +73,21 @@ public class TestReturnCommand {
         Expectation: All inputs should be able to be handled, no book should
         be returned
          */
-        Mockito.doReturn(visitorMock).when(retSpy).queryVisitor(any());
-        Mockito.doReturn(Collections.<Book>emptySet()).when(retSpy).queryBooks(any());
+        Mockito.doReturn(visitorMock).when(retSpy).queryVisitor(Matchers.anyLong());
+        Mockito.doReturn(Collections.<Book>emptySet()).when(retSpy).queryBooks(Mockito.any());
         Assertions.assertSame(ResponseFlag.SUCCESS,
                 retSpy.onExecute(this.execMock, "1", "1"));
-        Mockito.verify(execMock).sendMessage("return,invalid-book-id,2");
+        Mockito.verify(execMock).sendMessage("return,invalid-book-id,1;");
+    }
+
+    @Test
+    public void happyPath(){
+        Mockito.doReturn(visitorMock).when(retSpy).queryVisitor(Matchers.anyLong());
+        Book book = Mockito.mock(Book.class);
+        Checkout checkout = Mockito.mock(Checkout.class);
+        Mockito.doReturn(Collections.<Book>singleton(book)).when(retSpy).queryBooks(Mockito.any());
+        Mockito.doReturn(Collections.<Checkout>singletonList(checkout)).when(retSpy).queryCheckouts(Matchers.any(), Matchers.any());
+        Assertions.assertSame(ResponseFlag.SUCCESS, retSpy.onExecute(this.execMock, "1", "1"));
+        Mockito.verify(execMock).sendMessage("return,success;");
     }
 }
